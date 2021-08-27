f4514edb5bb5a66c359c7f2b881bf5e1ccd20ad1
Linus-Peng1 <snoopypeng1217@gmail.com>
2021-08-27 13:56:35 +0800

    feat: install express / pug / mysql

--- students/linus/Stylish/app.js
+++ students/linus/Stylish/app.js
@@ -1,10 +1,30 @@
 const express = require('express')
-const app = express()
+const bodyParser = require('body-parser')
+const mysql = require('mysql')
 
-app.get('/', (req, res) => {
-  res.send('Hellow World')
+const routes = require('./routes')
+
+const app = express()
+const PORT = 3000
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "stylish"
+})
+db.connect(err => {
+  if (err) throw err;
+  console.log("資料庫連線成功")
 })
 
-app.listen(3000, () => {
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static('public'))
+
+app.set('view engine', 'pug')
+
+app.use(routes)
+
+app.listen(PORT, () => {
   console.log('Server running on port 3000')
 })
